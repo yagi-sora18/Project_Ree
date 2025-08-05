b04bbdcbfe3f7ac739cb29af06ec03ec0f35a8e8
Shingo-sueyoshi <138412610+Shingo-sueyoshi@users.noreply.github.com>
2025-08-05 11:09:33 +0900

    InGame/AppMainの修正

--- Ree_Project/AppMain.cpp
+++ Ree_Project/AppMain.cpp
@@ -1,50 +1,20 @@
 #include "DxLib.h"
-#include "Utillity/InputControl.h"
-#include "Object/Player/Player.h"
-#include "Object/Platform/Platform.h"
-#include "Object/Item/Coin.h"
-
-#include "Map/MapLoader.h"
-
-
-
-const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
-
+#include "SceneManager.h"
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
 {
+    // 画面設定
     ChangeWindowMode(TRUE);
-    SetGraphMode(SCREEN_WIDTH, SCREEN_HEIGHT, 32);
+    SetGraphMode(640, 480, 32);
     if (DxLib_Init() == -1) return -1;
     SetDrawScreen(DX_SCREEN_BACK);
 
-    Player player;
-
-    std::vector<Platform> platforms;
-    std::vector<Coin> coins;
-
-    LoadMapFromCSV("Resource/Map/map01.csv", platforms, coins);
-
-    int camera_y = 0;
-
+    // ゲームループ
     while (ProcessMessage() == 0 && ClearDrawScreen() == 0)
     {
-        InputControl::GetInstance()->Update();
-        player.Update();
-        player.ApplyPhysics(platforms);
-
-        // カメラY追従（上下）
-        camera_y = static_cast<int>(player.pos.y) - SCREEN_HEIGHT / 2;
-        if (camera_y < 0) camera_y = 0;
-
-
-        // 地面描画
-
-        for (auto& p : platforms) p.Draw(camera_y);
-        for (auto& c : coins) c.Draw(camera_y);
-
-        player.Draw(camera_y);
+        // 現在のシーンを更新・描画
+        SceneManager::GetInstance()->Update();
+        SceneManager::GetInstance()->Draw();
 
         ScreenFlip();
     }
--- Ree_Project/Scene/InGame.h
+++ Ree_Project/Scene/InGame.h
@@ -1,5 +1,21 @@
 #pragma once
-class InGame
-{
-};
+#include "SceneBase.h"
+#include "../Object/Player/Player.h"
+#include "../Object/Platform/Platform.h"
+#include "../Object/Item/Coin.h"
 
+class InGame : public SceneBase
+{
+public:
+    InGame();
+    ~InGame();
+
+    void Update() override;
+    void Draw() override;
+
+private:
+    Player player;
+    std::vector<Platform> platforms;
+    std::vector<Coin> coins;
+    int camera_y;
+};
