5b73e9dc4271bf9e570db2c6ad374579bade6b19
yagi-sora18 <sora-yagi-k23f@stu.kbc.ac.jp>
2025-08-05 14:32:59 +0900

    Revert "遷移"
    This reverts commit 9a2c60e4f2fad905dca274749b0b40a1bff5cec8.

--- Ree_Project/Scene/End.cpp
+++
@@ -1,1 +0,0 @@
-#include "End.h"
--- Ree_Project/Scene/SceneManager.h
+++ Ree_Project/Scene/SceneManager.h
@@ -1,32 +1,5 @@
 #pragma once
-
-#include "SceneBase.h"
-
-// シーン管理クラス
 class SceneManager
 {
-private:
-	// 現在のシーン
-	SceneBase* current_scene;
-	// ゲームループを行うかのフラグ
-	bool loop_flag;
-
-public:
-	SceneManager();
-	~SceneManager();
-
-public:
-	void Initialize();
-	void Update();
-	void Finalize();
-
-public:
-	// ゲームループを行うか調べる処理
-	bool LoopCheck() const;
-
-private:
-	// シーン切り替え処理
-	void ChangeScene(eSceneType new_scene_type);
-	// シーン生成処理
-	SceneBase* CreateScene(eSceneType new_scene_type);
 };
+
